f351f2b891fd62cfdc2ad783f3f488dab99a6c35
Erick F <erickflores785@gmail.com>
2018-07-12 17:46:48 -0500

    Waiting for wallpaper to readjust template tags

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -8,11 +8,22 @@ import { QuizService } from '../quiz.service';
 })
 export class ResultComponent implements OnInit {
 
+  // STRING DATA FOR TITLE
+  title: string = "Your Journey has Come to an End";
+
+  // STRING DATA FOR QUOTE
+  paragraph: string = `"The most important reason for going from one place to another is to see what's in between, and they took great
+       pleasure in doing just that."`;
+  author: string = "― Norton Juster, The Phantom Tollbooth ";
+
+  // STRING DATA FOR RESULT
+  answer: string = "Superman";
+
   constructor(private quiz: QuizService) {
     this.quiz = quiz;
     // shows description defined in the QuizService
     console.log(this.quiz.description);
-   }
+  }
 
   ngOnInit() {
   }
