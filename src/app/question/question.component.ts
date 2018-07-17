5cf854c2af808a94d8baf07941cedfa5f9531c5d
mike.zapata283@gmail.com <mike.zapata283@gmail.com>
2018-07-17 17:31:00 -0500

    questions update

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { QuizService } from '../quiz.service';
 
 @Component({
@@ -6,14 +6,15 @@ import { QuizService } from '../quiz.service';
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent implements OnInit {
+export class QuestionComponent {
 
-  constructor(private quiz: QuizService) { 
+  constructor(private quiz: QuizService) {
     this.quiz = quiz;
     // shows description defined in the QuizService
+
   }
-    
-  ngOnInit() {
-  }
+
+
+
 
 }
--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,139 +6,180 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class QuizService {
+
+  morality: boolean;
+
+
   constructor(private router: Router) {
     this.router = router;
    }
   // All your quiz logic exists here, Define your vairable here
-  
-selectAnswer() {
-  
-}
+
+  // selectAnswer() {
+  //
+  // }
+
+   students = [
+     {
+       name: 'Josue',
+       age: 25,
+       morality: false
+     },
+     {
+       name: 'Erick',
+       age: 20,
+       morality: false
+     },
+     {
+       name: 'Michael',
+       age: 35,
+       morality: true
+     }
+   ];
+
+
+
+
+
 
   questions = [
     {
       id: 1,
-      title: "Would you rather...",
+      title: 'Would you rather...',
       selectAnswer: "",
       choices: [
         {
-          answer: "Die as a Hero",
+          answer: 'Die as a Hero',
           value: true
         },
         {
-          answer: "Live long enough to become the Villain",
+          answer: 'Live long enough to become the Villain',
           value: false
         },
-        ]
+      ]
     },
-    {
-      id: 2,  
-      title: "Where do you call home?",
+  {
+      id: 2,
+      morality: true,
+      title: 'Where do you call home?',
+      selectAnswer: null,
         choices: [
           {
-          answer: "The 3rd planet in the Solar System",
-          value: null
+          answer: 'The 3rd planet in the Solar System',
+          value: 'A'
           },
           {
-            answer: "In an Urban Area",
-            value: null
+            answer: 'In an Urban Area',
+            value: 'B'
           },
           {
-            answer: "Isoleted from society",
-            value: null
+            answer: 'Isoleted from society',
+            value: 'C'
           },
         ]
     },
     {
       id: 3,
+      morality: false,
       title: "Where do you call home?",
+      selectAnswer: null,
       choices: [
         {
         answer: "Penthouse",
-        value: null
+        value: "D"
       },
       {
         answer: "An Asylum",
-        value: null
+        value: "E"
       },
       {
         answer: "The Jungle",
-        value: null
+        value: "F"
       },
       ]
     },
     {
       id: 4,
+      morality: true,
       title: "How do you force your will?",
+      selectAnswer: null,
       choices: [
         {
           answer: "WTF SUPERPOWERS",
-          value: null
+          value: "A"
         },
         {
           answer: "Gadgets Galore",
-          value: null
+          value: "B"
         },
         {
           answer: "Warrior, Brute Force",
-          value: null
+          value: "C"
         },
         ]
     },
     {
         id: 5,
+        morality: false,
         title: "How do you force your will?",
+      selectAnswer: null,
         choices: [
           {
           answer: "Money & Wise-Ass mind",
-          value: null
+          value: "D"
           },
           {
             answer: "Sadistic Mindset",
-            value: null
+            value: "E"
           },
           {
             answer: "Anamilistic",
-            value: null
+            value: "F"
           },
         ]
       },
       {
         id: 6,
+        morality: true,
         title: "How do you get around town?",
+        selectAnswer: null,
         choices: [
           {
           answer: "Fly like a Bird",
-          value: null
+          value: "A"
           },
           {
             answer: "Badass Car",
-            value: null
+            value: "B"
           },
           {
             answer: "See through Jet",
-            value: null
+            value: "C"
           },
         ]
       },
       {
         id: 7,
+        morality: false,
         title: "How do you get around town?",
+        selectAnswer: null,
         choices: [
           {
             answer: "Limo",
-            value: null
+            value: "D"
           },
           {
             answer: "Whatever car you can get your hands on",
-            value: null
+            value: "E"
           },
           {
             answer: "Walk it like you talk it",
-            value: null
+            value: "F"
           },
         ]
       },
   ];
 
+  // selectAnswer() {
+  //   this.message = '';
 }
